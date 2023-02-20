965da15a628b391348070da7f68711f54a05c865
tokarevsas31 <alexander.tokarev@urfu.ru>
2023-02-20 16:50:10 +0500

    clone

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+import json
+
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis") 
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text)[0]
+
---
+++ test_main.py
@@ -0,0 +1,31 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+
+def test_read_main():
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_positive():
+        response = client.post("/predict/",
+                json={"text": "I love linux!"}
+        )
+        json_data = response.json()
+
+        assert response.status_code == 200
+        assert json_data['label'] == 'POSITIVE'
+
+def test_predict_negative():
+        response = client.post("/predict/",
+                json={"text": "I hate linux!"}
+        )
+        json_data = response.json()
+
+        assert response.status_code == 200
+        assert json_data['label'] == 'NEGATIVE'
+
